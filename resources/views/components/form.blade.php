10d142639be96b58320cee52f45e37fc22a9027b
Nikola <perisicnikola37@gmail.com>
2024-03-18 16:21:18 +0100

    Keep old form validation input values

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -3,7 +3,7 @@
 <div class="mx-auto">
     <div>
         <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-        <input type="text" id="title" name="title" value="{{ $initialValues['title'] ?? '' }}"
+        <input type="text" id="title" name="title" value="{{ old('title', $initialValues['title'] ?? '') }}"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[60%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @if ($errors->has('title')) border-red-500 @endif"
             placeholder="Title" />
         @error('title')
@@ -14,7 +14,8 @@
     <div>
         <label for="description"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-        <input type="text" id="description" name="description" value="{{ $initialValues['description'] ?? '' }}"
+        <input type="text" id="description" name="description"
+            value="{{ old('description', $initialValues['description'] ?? '') }}"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[60%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @if ($errors->has('description')) border-red-500 @endif"
             placeholder="Description" />
         @error('description')
--- resources/views/layouts/components/header.blade.php
+++ resources/views/layouts/components/header.blade.php
@@ -18,8 +18,8 @@
         <!-- Avatar and user name on the right -->
         <div class="flex items-center space-x-4">
             <!-- Avatar -->
-            <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D"
-                alt="Avatar" class="h-10 w-10 rounded-full">
+            <img src="https://avatars.githubusercontent.com/u/79047182?v=4" alt="Avatar"
+                class="h-10 w-10 rounded-full">
             <!-- User name -->
             <span class="text-lg font-semibold">{{ Auth::user()->name }}</span>
         </div>
